74a14471f34b44546cfd68342e5d284fc6d4e42a
Natasha <njohnson533@gmail.com>
2022-01-06 18:14:49 -0500

    added sticky header functionality

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,16 @@ const hamburgerBtn = document.getElementById("hamburger");
 const line = document.getElementById("line");
 const navLinks = document.getElementById("nav-links");
 const scrollBtn = document.getElementById("scroll");
+const header = document.querySelector(".header");
 
 const toggleMenu = () => {
     line.classList.toggle("open-hamburger");
     navLinks.classList.toggle("show-links");
 }
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 70) {
