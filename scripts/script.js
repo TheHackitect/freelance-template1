9e3f091fc053f3ebf626d786cdde91116df8119a
Natasha <njohnson533@gmail.com>
2022-01-06 00:25:13 -0500

    added media queries for header

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 const hamburgerBtn = document.getElementById("hamburger");
 const line = document.getElementById("line");
+const navLinks = document.getElementById("nav-links");
 
 const toggleMenu = () => {
     //line.classList.toggle("hide");
     line.classList.toggle("open-hamburger");
+    navLinks.classList.toggle("show-links");
 }
 
 hamburgerBtn.addEventListener("click", toggleMenu);
