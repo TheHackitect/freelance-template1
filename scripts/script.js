2712739bd0b87dfac5cc735b82f5c2f514fe69dc
Natasha <njohnson533@gmail.com>
2022-01-06 17:09:08 -0500

    add js functionality for scroll to top button

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,23 @@
 const hamburgerBtn = document.getElementById("hamburger");
 const line = document.getElementById("line");
 const navLinks = document.getElementById("nav-links");
+const scrollBtn = document.getElementById("scroll");
 
 const toggleMenu = () => {
     line.classList.toggle("open-hamburger");
     navLinks.classList.toggle("show-links");
 }
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 70) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+scrollBtn.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0;
+});
 
 hamburgerBtn.addEventListener("click", toggleMenu);
