43fc73a5fa4debda875bb389bd94f1f84e79a0b4
Natasha <njohnson533@gmail.com>
2022-01-05 22:42:28 -0500

    fixed hamburger animation, removed commented out code

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,8 @@ const hamburgerBtn = document.getElementById("hamburger");
 const line = document.getElementById("line");
 
 const toggleMenu = () => {
-    line.classList.add("hide");
+    //line.classList.toggle("hide");
+    line.classList.toggle("open-hamburger");
 }
 
 hamburgerBtn.addEventListener("click", toggleMenu);
